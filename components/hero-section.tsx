18b8167c2fe80dd67c72818c21f900e0844cb721
phthalos <phthalos64@gmail.com>
2025-07-27 04:04:35 +0900

    hero section 모바일 버전 디자인 수정

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,12 +6,12 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 export default function HeroSection() {
     return (
         <>
-            <div className="w-full h-96 overflow-hidden rounded-3xl flex">
-                <div className="absolute self-center text-white text-shadow-[0_0px_5px_rgb(0_0_0_/_0.75)] ml-16 lg:ml-32 flex flex-col gap-2">
+            <div className="w-full lg:h-96 md:h-80 h-42 overflow-hidden rounded-3xl flex">
+                <div className="absolute self-center text-white text-shadow-[0_0px_5px_rgb(0_0_0_/_0.75)] ml-16 lg:ml-32 flex flex-col md:gap-2">
                     {/* 텍스트 */}
-                    <TypographyLarge>새로운 공지사항</TypographyLarge>
-                    <TypographyH1>코마 웹사이트 오픈</TypographyH1>
-                    <TypographyP>
+                    <TypographyLarge className="text-sm lg:text-lg">새로운 공지사항</TypographyLarge>
+                    <TypographyH1 className="text-xl md:text-3xl lg:text-4xl">코마 웹사이트 오픈</TypographyH1>
+                    <TypographyP className="hidden md:block">
                         새로운 공지사항 핵심 요약 1줄!
                         <br />
                         2번째 줄은 br 태그를 사용해서 작성합니다.
@@ -32,7 +32,8 @@ export default function HeroSection() {
                 {/* 배너 이미지 */}
                 <Image className="h-full object-cover" src={hero1} alt="Photo by Nidhin" priority />
             </div>
-            <ul className="self-center w-3/4 h-20 -mt-10 backdrop-blur bg-muted/90 supports-[backdrop-filter]:bg-muted/60 rounded-3xl items-center grid lg:grid-cols-4 grid-cols-1 text-center text-muted-foreground">
+            {/* 페이지네이션 */}
+            <ul className="self-center w-4/6 h-20 -mt-14 backdrop-blur bg-muted/90 supports-[backdrop-filter]:bg-muted/60 rounded-3xl items-center hidden lg:grid lg:grid-cols-4 grid-cols-1 text-center text-muted-foreground">
                 <li className="text-primary">코마 웹사이트 오픈</li>
                 <li>신규 부원 모집 중</li>
                 <li>신규 스터디 &apos;모각코&apos; 개설</li>
