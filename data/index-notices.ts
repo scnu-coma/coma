183f571e261f51eb1d62dca1a55bcfa64676b69f
phthalos <phthalos64@gmail.com>
2025-08-14 20:26:27 +0900

    메인배너 - 코마 웹사이트 오픈, 부원모집안내

--- data/index-notices.ts
+++ data/index-notices.ts
@@ -2,38 +2,23 @@
 // 4개만 쓰세요
 
 import hero1 from "@/public/images/nidhin-mohan-p_wC_T2HUPk-unsplash.jpg";
+import hero2 from "@/public/images/banner_recruitment.webp";
 
 export const notices = [
     {
         subtitle: "새로운 공지사항",
         title: "코마 웹사이트 오픈",
-        content: "새로운 공지사항 핵심 요약 1줄!",
-        content2: "2번째 줄은 br 태그를 사용해서 작성합니다.",
+        content: "코마 공식 홈페이지가 새롭게 문을 열었습니다.",
+        content2: "여러분의 많은 관심과 활용 부탁드립니다.",
         image: hero1,
-        href: "/notice/1",
+        href: "/notice/코마-웹사이트-오픈",
     },
     {
         subtitle: "부원 모집안내",
-        title: "신규 부원 모집 중",
-        content: "공지사항 요약 2번",
-        content2: "2",
-        image: hero1,
+        title: "2학기 신규 부원 모집",
+        content: "2025년 2학기 신규 부원 모집이 진행중입니다.",
+        content2: "",
+        image: hero2,
         href: "/recruitment/2025-2",
-    },
-    {
-        subtitle: "공지사항 3번",
-        title: "신규 스터디 '모각코' 개설",
-        content: "공지사항 요약 3번",
-        content2: "3",
-        image: hero1,
-        href: "/notice/3",
-    },
-    {
-        subtitle: "공지사항 4번",
-        title: "2학기 운영계획 안내",
-        content: "공지사항 요약 4번",
-        content2: "4",
-        image: hero1,
-        href: "/notice/4",
     },
 ];
