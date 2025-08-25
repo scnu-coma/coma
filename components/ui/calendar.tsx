f1a2cc9ff6dc253c552d35b22c20b2f0981f0a77
phthalos <phthalos64@gmail.com>
2025-08-25 19:04:04 +0900

    이번 학기 행사 달력 개별날짜 클릭방지

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -24,6 +24,7 @@ function Calendar({
     return (
         <DayPicker
             showOutsideDays={showOutsideDays}
+            disabled
             className={cn(
                 "bg-background group/calendar p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
                 String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
@@ -94,7 +95,7 @@ function Calendar({
                     defaultClassNames.today
                 ),
                 outside: cn("text-muted-foreground aria-selected:text-muted-foreground", defaultClassNames.outside),
-                disabled: cn("text-muted-foreground opacity-50", defaultClassNames.disabled),
+                // disabled: cn("text-muted-foreground opacity-50", defaultClassNames.disabled),
                 hidden: cn("invisible", defaultClassNames.hidden),
                 ...classNames,
             }}
@@ -151,7 +152,7 @@ function CalendarDayButton({ className, day, modifiers, ...props }: React.Compon
             data-range-end={modifiers.range_end}
             data-range-middle={modifiers.range_middle}
             className={cn(
-                "data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground flex aspect-square size-auto w-full min-w-(--cell-size) flex-col gap-1 leading-none font-normal group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70",
+                "data-[selected-single=true]:bg-primary data-[selected-single=true]:text-primary-foreground data-[range-middle=true]:bg-accent data-[range-middle=true]:text-accent-foreground data-[range-start=true]:bg-primary data-[range-start=true]:text-primary-foreground data-[range-end=true]:bg-primary data-[range-end=true]:text-primary-foreground group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground flex aspect-square size-auto w-full min-w-(--cell-size) flex-col gap-1 leading-none font-normal group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:ring-[3px] data-[range-end=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-start=true]:rounded-l-md [&>span]:text-xs [&>span]:opacity-70 opacity-100!",
                 defaultClassNames.day,
                 className
             )}
